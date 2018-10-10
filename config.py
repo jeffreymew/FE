4157c1bb6a0cd069efe29b6862c9d1055dab0aec
Jeffrey Mew <t-jemew@microsoft.com>
2018-10-10 14:56:55 -0700

    test

--- config.py
+++ config.py
@@ -8,12 +8,3 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgres://postgres@standup-db:Password123!@standup-db.postgres.database.azure.com:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-
-class TestingConfig(object):
-    """Development configuration."""
-    TESTING = True
-    DEBUG = True
-    WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    DEBUG_TB_ENABLED = True
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
